c6045cc62015a996d45eb8ae83f5e7360927de16
Camilo <camiloet98@gmail.com>
2025-12-11 10:00:49 -0500

    up

--- servicios/src/main/java/com/ui/main/config/WebCorsConfig.java
+++ servicios/src/main/java/com/ui/main/config/WebCorsConfig.java
@@ -17,9 +17,9 @@ public class WebCorsConfig {
 
         // Orígenes explícitos
         cors.setAllowedOrigins(List.of(
-                "http://40.67.148.88",
-                "http://datos.urbanik-hub.com",// frontend en Azure (puerto 80)
-                "https://datos.urbanik-hub.com",// frontend en Azure (puerto 80)
+                "http://20.246.65.7",
+                "http://metro.urbanik-hub.com",// frontend en Azure (puerto 80)
+                "https://metro.urbanik-hub.com",// frontend en Azure (puerto 80)
                 "http://localhost:5173",    // dev Vite (si usas)
                 "http://127.0.0.1:5173"
         ));
